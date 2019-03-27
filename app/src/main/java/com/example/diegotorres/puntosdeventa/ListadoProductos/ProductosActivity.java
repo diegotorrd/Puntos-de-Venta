75e594b8bcc786d79af21af426a651cf906c11f4
Diego Torres Esquerre <diego40615@gmail.com>
2019-03-26 23:43:22 -0500

    Se agrego selector multiple al ListView de productos

--- app/src/main/java/com/example/diegotorres/puntosdeventa/ListadoProductos/ProductosActivity.java
+++ app/src/main/java/com/example/diegotorres/puntosdeventa/ListadoProductos/ProductosActivity.java
@@ -4,7 +4,14 @@ import android.content.Context;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,6 +41,7 @@ public class ProductosActivity extends AppCompatActivity implements ProductosVie
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productos);
+
         mListViewProductos = (ListView) findViewById(R.id.list_productos);
         mPlaceId = getIntent().getStringExtra("placeId").toString();
         mProductos = new ArrayList<>();
@@ -71,6 +79,49 @@ public class ProductosActivity extends AppCompatActivity implements ProductosVie
                 mBuilder.setView(mView);
                 mDialog = mBuilder.create();
                 mDialog.show();
+            }
+        });
+
+        mListViewProductos.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE_MODAL);
+        mListViewProductos.setMultiChoiceModeListener(new AbsListView.MultiChoiceModeListener() {
+            @Override
+            public void onItemCheckedStateChanged(ActionMode actionMode, int i, long l, boolean b) {
+                actionMode.setTitle(""+mListViewProductos.getCheckedItemCount() + " seleccionados");
+            }
+
+            @Override
+            public boolean onCreateActionMode(ActionMode actionMode, Menu menu) {
+                MenuInflater menuInflater = actionMode.getMenuInflater();
+                menuInflater.inflate(R.menu.menu_select, menu);
+                return true;
+            }
+
+            @Override
+            public boolean onPrepareActionMode(ActionMode actionMode, Menu menu) {
+                return false;
+            }
+
+            @Override
+            public boolean onActionItemClicked(ActionMode actionMode, MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+
+                    case R.id.menu_delete:
+
+                        SparseBooleanArray checkedItems = mListViewProductos.getCheckedItemPositions();
+                        for(int i =0;i<checkedItems.size();i++){
+                            if(checkedItems.valueAt(i) == true){
+                                mProductos.remove(i);
+                            }
+                        }
+                        mAdapter.notifyDataSetChanged();
+                        actionMode.finish();
+                }
+                return true;
+            }
+
+            @Override
+            public void onDestroyActionMode(ActionMode actionMode) {
+
             }
         });
 
